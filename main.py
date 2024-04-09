b3c320b93c46fce26a03a3c1750f20cc163dae4f
Pulkit Jalan <pulkit1990@gmail.com>
2024-04-09 22:58:42 +0100

    feat: add environment arg

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ import CloudFlare
 parser = argparse.ArgumentParser(description='Purge Cloudflare Pages cache')
 parser.add_argument('--account', required=True, help='Account ID')
 parser.add_argument('--project', required=True, help='Pages project name to purge')
+parser.add_argument('--environment', default='preview', choices=['production', 'preview'], help='The environment to purge')
 parser.add_argument('--keep', type=int, default=3, help='Number of deployments to keep')
 
 args = parser.parse_args()
 
 cf = CloudFlare.CloudFlare()
-deployments = cf.accounts.pages.projects.deployments.get(args.account, args.project)
+deployments = cf.accounts.pages.projects.deployments.get(args.account, args.project, params = {'env': args.environment})
 deployments = deployments[args.keep:]
 
 for deployment in deployments:
